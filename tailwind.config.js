26f2f97e52d8cf77ac2d2b048e3d9251cabfb8c6
mdsajalali <mdsajalali2020@gmail.com>
2025-06-18 23:31:07 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nantes: ["Nantes", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
